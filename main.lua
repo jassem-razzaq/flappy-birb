6353756d84808f257ea56c5d115970a7022a0a46
jassem-razzaq <jassem.razzaq@gmail.com>
2025-08-13 21:48:57 -0400

    add bg scroll and birb

--- main.lua
+++ main.lua
@@ -1,17 +1,78 @@
-local background
+--[[
+    BIRB GAME
+]]
+
+-- LIBRARIES
+push = require 'lib.push'
+Class = require 'lib.class'
+
+require 'src.Birb'
+
+
+-- physical screen dimensions
+WINDOW_WIDTH = 360
+WINDOW_HEIGHT = 540
+
+-- virtual resolution dimensions
+VIRTUAL_WIDTH = 720
+VIRTUAL_HEIGHT = 1080
+
+-- images we load into memory from files to later draw onto the screen
+local background = love.graphics.newImage('assets/sprites/sky.png')
+local backgroundScroll = 0
+
+local ground = love.graphics.newImage('assets/sprites/ground.png')
+local groundScroll = 0
+
+local BACKGROUND_SCROLL_SPEED = 100
+local GROUND_SCROLL_SPEED = 200
+
+local BACKGROUND_LOOP_PT = 720
+local GROUND_LOOP_PT = 1440
+
+local birb = Birb()
+
 
 function love.load()
-    background = love.graphics.newImage("assets/sprites/background.png")
+    -- initialize our nearest-neighbor filter
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- app window title
+    love.window.setTitle('BIRB GAME')
+
+    -- initialize our virtual resolution
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })
 end
 
-function love.update()
+function love.resize(w, h)
+    push:resize(w, h)
+end
 
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.update(dt)
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOP_PT
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+
+    birb:update(dt)
 end
 
 function love.draw()
-  for i = 0, love.graphics.getWidth() / background:getWidth() do
-    for j = 0, love.graphics.getHeight() / background:getHeight() do
-      love.graphics.draw(background, i * background:getWidth(), j * background:getHeight())
-    end
-  end
+    push:start()
+    
+    -- draw the background starting at top left (0, 0)
+    love.graphics.draw(background, -backgroundScroll, 0)
+
+    -- draw the ground on top of the background, toward the bottom of the screen
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 110)
+    birb:render()
+    push:finish()
 end

842b22b350f861c61eb44f1d62dcc198d7185004
jassem-razzaq <jassem.razzaq@gmail.com>
2025-08-13 21:48:38 -0400

    add birb class

---
+++ src/Birb.lua
@@ -0,0 +1,23 @@
+Birb = Class{}
+
+local GRAVITY = 20
+
+function Birb:init()
+  self.image = love.graphics.newImage('assets/sprites/birb.png')
+  self.width = self.image:getWidth()
+  self.height = self.image:getHeight()
+
+  self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
+  self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
+
+  self.dy = 0
+end
+
+function Birb:update(dt)
+  self.dy = self.dy + GRAVITY * dt
+  self.y = self.y + self.dy
+end
+
+function Birb:render()
+  love.graphics.draw(self.image, self.x, self.y)
+end
